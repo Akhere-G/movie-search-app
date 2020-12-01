b16b91b145a0d3ad1d83b64b5f3cb23a55780b78
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-01 01:11:58 +0000

    added styles to error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <section className="section">
+    <section className='section'>
       <h2>Oops! Page not found</h2>
-      <div className="btn-container">
-        <Link to="/">
-          <button>go home</button>
+      <div className='btnContainer'>
+        <Link className='btn' to='/'>
+          go home
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
