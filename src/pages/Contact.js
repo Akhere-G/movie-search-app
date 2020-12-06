53d46829e6dc9572bec12c015120edb8ab8afca7
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-06 22:31:10 +0000

    small typo in contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
         <p>
           If you have any questions, I am happy to answer on my email. On my
           github, you can find many of my projects, web apps and also a few Java
-          projects I have done in my spare time. I look forward to hearing from
+          projects I have created in my spare time. I look forward to hearing from
           you.
         </p>
         <br />
